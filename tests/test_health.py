0565479e3038e39eec3fafcd525e6031e8744e85
Chintan Shah <chintan.shah@waitrose.co.uk>
2026-05-26 13:16:56 +0530

    Evidences for week 1 added

--- tests/test_health.py
+++ tests/test_health.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    assert response.json() == {"status": "k"}
